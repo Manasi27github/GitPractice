a81471c1fdfa8394d4a91d55fe45a422092071f7
Manasi <manasipadwal27@gmail.com>
2023-01-19 14:20:28 +0530

    new class added

---
+++ src/com/velocity/CampareArrayInSize.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+public class CampareArrayInSize {
+
+	//campare two array are equal in size or not
+		public static void main(String[] args) {
+	  int []a= {1,2,3,4,5};
+	  int []b= {7,8,9,3,5};
+	  if (a.length==b.length) {
+		  System.out.println("it is equal in size");
+	  }
+		  else {
+			  System.out.println("it is not equal in size");
+	 
+		  }
+	}
+
+}
